2597da70ccd741a2b3cd8c4635b94ba0f50b4aa5
PhyllisWong <pwong@gmail.com>
2018-05-24 09:15:08 -0700

    Update constructor from depreciated function call to constructor keyword

--- voting.sol
+++ voting.sol
@@ -1,5 +1,5 @@
-// We have to specify what version of compiler this code will compile with
 pragma solidity ^0.4.11;
+// We have to specify what version of compiler this code will compile with
 
 
 contract Voting {
@@ -11,32 +11,32 @@ contract Voting {
 
     /* Solidity doesn't allow you to pass in an array of strings in the constructor
     (yet). We will use an array of bytes32 instead to store the list of candidates.*/
-    bytes32[] public cadidatesList;
+    bytes32[] public candidatesList;
 
     /* This is the constructor that will be called once we deploy the contract
     to the blockchain. When we deploy the contract, we will pass an array of
     candidates in the election.*/
-    function Voting(bytes32[] candidateNames) {
-        cadidatesList = candidateNames;
+    constructor(bytes32[] candidateNames) {
+        candidatesList = candidateNames;
     }
 
     // This function returns the total votes a candidate has reveived so far.
     function  totalVotesFor(bytes32 candidate) returns (uint8) {
-        if (validCandidate(candidate) == false) throw;
+        if (validCandidate(candidate) == false) revert();
         return votesReceived[candidate];
     }
 
     /* This function increments the vote count for the specifies candidate.
     Equivalent to casting a vote. */
     function voteForCandidate(bytes32 candidate) {
-        if (validCandidate(candidate) == false) throw;
+        if (validCandidate(candidate) == false) revert();
         votesReceived[candidate] += 1;
     }
 
     // Validates if a candidate is valid
     function validCandidate(bytes32 candidate) returns (bool) {
-        for(uint i = 0; i < candidateList.length; i++) {
-            if (candidateList[i] == candidate) {
+        for(uint i = 0; i < candidatesList.length; i++) {
+            if (candidatesList[i] == candidate) {
                 return true;
             }
         }
